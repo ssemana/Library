8e0464d3f4d09b2dbe4d55473392092e92e45b82
Shannon Semana <ssemana@gmail.com>
2021-07-16 09:14:39 -0700

    initial setup

---
+++ javascript.js
@@ -0,0 +1,23 @@
+let myLibrary = []
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.status = status;
+    
+    function bookInfo(){
+      return title + " by " + author + ", " + "pages" + ", " + read " ."
+    }
+    
+  }
+}
+
+function addBooktoLibrary() {
+
+}
+
+function displayBook {
+
+}
